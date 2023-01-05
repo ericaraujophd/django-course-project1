d0e1a6139c854ced0b68a4f2c84e0ac2827da3a8
Eric Araujo <eric@ufla.br>
2023-01-05 02:28:42 -0300

    Movendo as views para o app recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    # return HTTP Response
+    return HttpResponse("Home")
+
+
+def sobre(request):
+    # return HTTP Response
+    return HttpResponse("Sobre")
+
+
+def contato(request):
+    # return HTTP Response
+    return HttpResponse("Contato")
